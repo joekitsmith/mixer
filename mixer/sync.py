20160c9c7c2d9c3598697ba6071f986487d02d9e
joekitsmith <joesmith29@hotmail.co.uk>
2023-10-11 20:49:21 +0100

    Migrate to librosa

--- mixer/sync.py
+++ mixer/sync.py
@@ -1,9 +1,11 @@
-from logger import logger
 import pathlib
+
+import librosa
 import numpy as np
 import torch
 import torchaudio
-from madmom.features.downbeats import DBNDownBeatTrackingProcessor, RNNDownBeatProcessor
+from logger import logger
+
 
 def main():
     prev_audio_path = pathlib.Path("./data/track1.mp3")
@@ -19,7 +21,7 @@ def main():
     logger.info(f"BPM for {next_audio_path.stem} is {round(next_bpm, 2)}")
 
 
-def load_audio(audio_path: pathlib.Path) -> torch.Tensor:
+def load_audio(audio_path: pathlib.Path) -> np.ndarray:
     """
     Load audio file into mono torch Tensor.
 
@@ -30,26 +32,27 @@ def load_audio(audio_path: pathlib.Path) -> torch.Tensor:
 
     Returns
     -------
-    audio : torch.Tensor
-        tensor of shape (1, y)
+    audio : np.ndarray
+        array of shape ?
     """
     if audio_path.suffix == "mp3":
-        torchaudio.set_audio_backend('soundfile')
+        torchaudio.set_audio_backend("soundfile")
 
     audio = torchaudio.load(audio_path)[0]
     audio = to_mono(audio)
+    audio = squeeze_dim(audio).numpy()
 
     return audio
 
 
-def estimate_beat(audio: torch.Tensor) -> tuple[float, np.ndarray]:
+def estimate_beat(audio: np.ndarray) -> tuple[float, np.ndarray]:
     """
     Estimate bpm and downbeat times for a given mono audio tensor.
 
     Parameters
     ----------
-    audio : torch.Tensor
-        tensor of shape (1, y)
+    audio : np.ndarray
+        array of shape ?
 
     Returns
     -------
@@ -58,46 +61,8 @@ def estimate_beat(audio: torch.Tensor) -> tuple[float, np.ndarray]:
     downbeats : np.ndarray
         positions of downbeats for every bar
     """
-    if isinstance(audio, torch.Tensor): 
-        audio = squeeze_dim(audio).numpy()
+    return librosa.beat.beat_track(y=audio)
 
-    proc = DBNDownBeatTrackingProcessor(beats_per_bar=[3, 4], fps=100)
-
-    logger.info("Getting joint beat and downbeat activation function")
-    act = RNNDownBeatProcessor()(audio)
-    
-    logger.info("Obtaining beat positions using beat activation function")
-    proc_res = proc(act)
-    
-    downbeats = proc_res[proc_res[:,1] == 1, 0]
-    beat_curve = proc_res[:, 0]
-    bpm = estimate_bpm(beat_curve)
-
-    return bpm, downbeats
-
-def estimate_bpm(beat_curve: np.ndarray) -> float:
-    """
-    Estimate tempo from beat times by sampling the middle third of an audio track.
-
-    Parameters
-    ----------
-    beat_curve : np.ndarray
-        times of every beat in every bar
-
-    Returns
-    -------
-    bpm : float
-        tempo of audio in beats per minute
-    """
-    total_beat = len(beat_curve)
-
-    sample_start = int(total_beat / 3)
-    sample_end = int(total_beat * 2 / 3)
-    sample_beat_num = sample_end - sample_start
-    sample_time = beat_curve[sample_end] - beat_curve[sample_start]
-
-    bpm = float(sample_beat_num / (sample_time / 60))
-    return bpm
 
 def to_mono(audio: torch.Tensor, dim: int = -2) -> torch.Tensor:
     """
@@ -119,7 +84,8 @@ def to_mono(audio: torch.Tensor, dim: int = -2) -> torch.Tensor:
         return torch.mean(audio, dim=dim, keepdim=True)
     else:
         return audio
-    
+
+
 def squeeze_dim(audio: torch.Tensor) -> torch.Tensor:
     """
     Flatten multi-dimensional audio tensor to single dimension.
@@ -138,6 +104,7 @@ def squeeze_dim(audio: torch.Tensor) -> torch.Tensor:
     for dim in dims:
         audio = audio.squeeze(dim)
     return audio
+
 
 if __name__ == "__main__":
     main()
