021fe709dd0860f6047d19d6ee2a70e0c6699bb8
joekitsmith <joesmith29@hotmail.co.uk>
2023-10-30 16:47:41 +0000

    Mix several tracks and refactor

--- mixer/sync.py
+++ mixer/sync.py
@@ -1,274 +1,119 @@
-import pathlib
-from typing import Optional, Tuple
+import os
+from typing import cast
 
 import essentia.standard as es
 import matplotlib.pyplot as plt
 import numpy as np
-import pyrubberband as pyrb
 import soundfile as sf
-from madmom.features.downbeats import DBNDownBeatTrackingProcessor, RNNDownBeatProcessor
 
-SAMPLE_RATE = 44100  # Sample rate fixed for essentia
+from mixer.mix import Mix
+from mixer.track import SAMPLE_RATE, Track, TrackGroup
 
 
 def main():
-    prev_audio_path = pathlib.Path("data/track5.mp3")
-    next_audio_path = pathlib.Path("data/track6.mp3")
-
-    prev_audio = load_audio(str(prev_audio_path.resolve()))
-    next_audio = load_audio(str(next_audio_path.resolve()))
-
-    prev_audio = prev_audio[int(30 * SAMPLE_RATE) :]
-    next_audio = next_audio[int(30 * SAMPLE_RATE) :]
-
-    prev_bpm, prev_downbeats = extract_bpm_and_downbeats(prev_audio)
-    next_bpm, next_downbeats = extract_bpm_and_downbeats(next_audio)
-
-    next_audio_stretch = stretch_audio(next_audio, prev_bpm, next_bpm)
-    next_bpm_stretch, next_downbeats_stretch = extract_bpm_and_downbeats(
-        next_audio_stretch
-    )
-
-    prev_audio_aligned, next_audio_aligned = overlap_tracks(
-        prev_audio,
-        prev_downbeats,
-        next_audio_stretch,
-        next_downbeats_stretch,
-        prev_overlap_cue=48,
-        next_overlap_cue=30,
-        overlap_length=8,
-    )
-
-    combined_audio = combine_audio_tracks(prev_audio_aligned, next_audio_aligned)
-
+    track_groups = evaluate_tracklist()
+    track_group = max(track_groups, key=lambda x: len(x.tracks))
+    combined_audio = mix_track_group(track_group)
     sf.write("combined.mp3", combined_audio, int(SAMPLE_RATE))
 
 
-def load_audio(path: str) -> np.ndarray:
+def mix_track_group(track_group: TrackGroup) -> np.ndarray:
     """
-    Load an audio file from a given path.
+    Mix a group of tracks to their average tempo.
+    If no average tempo for track group, mix to default tempo value.
 
     Parameters
     ----------
-    path : str
-        local path to audio file
+    track_group : TrackGroup
+        a group of tracks with similar tempos
 
     Returns
     -------
     np.ndarray
-        mono representation of audio file
+        a mono audio signal with input tracks mixed at their average tempo
     """
-    loader = es.MonoLoader(filename=path, sampleRate=SAMPLE_RATE)
-    return loader()
+    bpm = track_group.bpm
+    if bpm is None:
+        bpm = 130.0
+
+    mix = Mix(bpm)
+
+    for i, track in enumerate(track_group.tracks):
+        track.crop(0, 90)
+
+        if i == 0:
+            mix.add_track(track, 30, 60)
+            continue
+
+        mix.add_track(track, 30, 60)
+
+    return mix.audio
 
 
-def extract_bpm_and_downbeats(audio: np.ndarray) -> Tuple[float, np.ndarray]:
+def evaluate_tracklist(tracklist_dir: str = "./data") -> list[TrackGroup]:
     """
-    Determine BPM and downbeat locations for audio
+    Identify tracks in an input directory and combine them into groups.
 
     Parameters
     ----------
-    audio : np.ndarray
-        mono representation of audio file
+    tracklist_dir : str
+        directory containing input tracks
 
     Returns
     -------
-    bpm : float
-        tempo of the audio file
-    downbeats : np.ndarray
-        time points of downbeats in audio file
+    track_groups : list[TrackGroups]
+        all tracks in directory bundled into groups with similar tempos
     """
-    # Extract BPM and beats
-    rhythm_extractor = es.RhythmExtractor2013(method="degara")
-    bpm, _, _, _, _ = rhythm_extractor(audio)
+    tracklist: list[Track] = []
+    for file in os.listdir(tracklist_dir):
+        if file.endswith(".mp3"):
+            track = Track(f"{tracklist_dir}/{file}")
+            track.load()
+            track.calculate_bpm()
+            tracklist.append(track)
 
-    downbeats = get_downbeats(audio)
+    track_groups = get_track_groups(tracklist)
 
-    return bpm, downbeats
+    return track_groups
 
 
-def stretch_audio(
-    audio: np.ndarray, original_bpm: float, target_bpm: float
-) -> np.ndarray:
+def get_track_groups(tracklist: list[Track], tempo_diff: int = 10) -> list[TrackGroup]:
     """
-    Time stretch audio file to increase BPM to target
+    Bundle tracks into groups based on how similar their tempos are.
 
     Parameters
     ----------
-    audio : np.ndarray
-        mono representation of audio file
-    original_bpm : float
-        BPM of audio before time stretching
-    target_bpm : float
-        intended BPM of audio
+    tracklist : list[Track]
+        input tracks
+    tempo_diff : int
+        maximum absolute difference in tempo between tracks in a group
 
     Returns
     -------
-    np.ndarray
-        time-stretched audio
+    groups : list[TrackGroup]
+        track groups for input tracks
     """
-    stretch_factor = original_bpm / target_bpm
-    return pyrb.time_stretch(audio, SAMPLE_RATE, stretch_factor)
+    tracklist_bpm_sorted = sorted(tracklist, key=lambda x: cast(float, x.bpm))
 
+    groups: list[TrackGroup] = []
+    for track in tracklist_bpm_sorted:
+        if track.bpm is not None:
+            if not groups:
+                group = TrackGroup()
+                group.add_track(track)
+                groups.append(group)
+            elif groups[-1].tracks[0].bpm is not None:
+                if track.bpm - groups[-1].tracks[0].bpm <= tempo_diff:
+                    groups[-1].tracks.append(track)
+                else:
+                    group = TrackGroup()
+                    group.add_track(track)
+                    groups.append(group)
 
-def align_on_beat(
-    prev_audio: np.ndarray,
-    prev_downbeats: np.ndarray,
-    next_audio: np.ndarray,
-    next_downbeats: np.ndarray,
-    prev_downbeat_offset: int = 0,
-    next_downbeat_offset: int = 0,
-    downbeat_adjust_factor: Optional[int] = None,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Align two audio tracks with the same tempo based on their downbeat information.
-    Each track can be started from its own defined downbeat.
-    The next audio track can be configured to start a fraction of a downbeat later than a whole beat.
+    for group in groups:
+        group.calculate_bpm()
 
-    Parameters
-    ----------
-    prev_audio : np.ndarray
-        mono representation of previous audio file
-    prev_downbeats : np.ndarray
-        time points of downbeats in previous audio file
-    next_audio : np.ndarray
-        mono representation of next audio file
-    next_downbeats : np.ndarray
-        time points of downbeats in next audio file
-    prev_downbeat_offset : int
-        index of downbeat to start previous audio file from
-    next_beat_offset : int
-        index of downbeat to start next audio file from
-    downbeat_adjust_factor : int
-        fraction of bar that next audio should be adjusted by
-        e.g. if set to 2, next audio will start 1/2 a bar later
-
-    Returns
-    -------
-    prev_audio_aligned : np.ndarray
-        mono representation of previous audio file aligned to next audio
-    next_audio_aligned : np.ndarray
-        mono representation of next audio file aligned to previous audio
-    """
-    # Find the start sample of first beat for each audio
-    prev_start_sample = int(prev_downbeats[prev_downbeat_offset] * SAMPLE_RATE)
-    next_start_sample = int(next_downbeats[next_downbeat_offset] * SAMPLE_RATE)
-
-    # Use numpy slicing to align both audios to start on their respective first downbeats
-    prev_audio_aligned = prev_audio[prev_start_sample:]
-    next_audio_aligned = next_audio[next_start_sample:]
-
-    if isinstance(downbeat_adjust_factor, int):
-        next_avg_downbeat_duration = np.mean(np.diff(next_downbeats))
-        half_downbeat_duration = next_avg_downbeat_duration / downbeat_adjust_factor
-        next_audio_aligned = next_audio_aligned[
-            int(half_downbeat_duration * SAMPLE_RATE) :
-        ]
-
-    return prev_audio_aligned, next_audio_aligned
-
-
-def overlap_tracks(
-    prev_audio: np.ndarray,
-    prev_downbeats: np.ndarray,
-    next_audio: np.ndarray,
-    next_downbeats: np.ndarray,
-    prev_overlap_cue: int,
-    next_overlap_cue: int,
-    overlap_length: int = 4,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Overlap two audio files at defined downbeat cue points for a defined overlap length.
-    Previous audio file is faded out during overlap region and next audio file is faded out.
-
-    Parameters
-    ----------
-    prev_audio : np.ndarray
-        mono representation of previous audio file
-    prev_downbeats : np.ndarray
-        time points of downbeats in previous audio file
-    next_audio : np.ndarray
-        mono representation of next audio file
-    next_downbeats : np.ndarray
-        time points of downbeats in next audio file
-    prev_overlap_cue : int
-        index of downbeat to use as starting cue point for previous audio file
-    next_overlap_cue : int
-        index of downbeat to use as starting cue point for next audio file
-    overlap_length : int
-        length of overlap region in bars
-
-    Returns
-    -------
-    prev_audio_faded : np.ndarray
-        previous audio file trimmed and fading applied
-    next_audio_faded : np.ndarray
-        next audio file trimmed and fading applied
-    """
-    # Find the start sample of first beat for each audio
-    prev_start_sample = int(
-        prev_downbeats[prev_overlap_cue - (2 * overlap_length)] * SAMPLE_RATE
-    )
-    prev_end_sample = int(
-        prev_downbeats[prev_overlap_cue + overlap_length] * SAMPLE_RATE
-    )
-    next_start_sample = int(
-        next_downbeats[next_overlap_cue - overlap_length] * SAMPLE_RATE
-    )
-    next_end_sample = int(
-        next_downbeats[next_overlap_cue + (3 * overlap_length)] * SAMPLE_RATE
-    )
-
-    prev_audio_trimmed = prev_audio[prev_start_sample : prev_end_sample + 1]
-    next_audio_trimmed = next_audio[next_start_sample : next_end_sample + 1]
-
-    # assume same SR here
-    fade_duration = next_end_sample - prev_start_sample
-    prev_audio_faded = fade_track(prev_audio_trimmed, fade_duration, mode="out")
-    next_audio_faded = fade_track(next_audio_trimmed, fade_duration, mode="in")
-
-    length_diff = len(next_audio_faded) - len(prev_audio_faded)
-
-    prev_audio_faded = np.pad(prev_audio_faded, (0, length_diff))
-    next_audio_faded = np.pad(next_audio_faded, (length_diff, 0))
-
-    return prev_audio_faded, next_audio_faded
-
-
-def combine_audio_tracks(
-    prev_audio: np.ndarray, next_audio: np.ndarray, normalise: bool = True
-) -> np.ndarray:
-    """
-    Combine two audio tracks into one audio track.
-
-    Parameters
-    ----------
-    prev_audio : np.ndarray
-        mono representation of previous audio file
-    next_audio : np.ndarray
-        mono representation of next audio file
-    normalise : bool
-        If True, amplitude of final audio track will be normalised between -1.0 and 1.0
-
-    Returns
-    -------
-    combined_audio : np.ndarray
-    """
-    # Pad the shorter audio with zeros at end so that they have the same length
-    length_diff = len(prev_audio) - len(next_audio)
-
-    if length_diff > 0:
-        next_audio = np.pad(next_audio, (0, length_diff))
-    elif length_diff < 0:
-        prev_audio = np.pad(prev_audio, (0, -length_diff))
-
-    combined_audio = prev_audio + next_audio
-
-    if normalise:
-        if np.max(np.abs(combined_audio)) > 1.0:
-            combined_audio /= np.max(np.abs(combined_audio))
-
-    return combined_audio
+    return groups
 
 
 def add_beat_markers_to_audio(audio: np.ndarray, beats: np.ndarray) -> np.ndarray:
@@ -321,62 +166,6 @@ def plot_beats(audio: np.ndarray, beats: np.ndarray, audio_name: str) -> None:
     plt.legend()
     plt.tight_layout()
     plt.show()
-
-
-def get_downbeats(audio: np.ndarray) -> np.ndarray:
-    """
-    Use madmom downbeat tracking to estimate downbeat time points for audio file.
-
-    Parameters
-    ----------
-    audio : np.ndarray
-        mono representation of audio file
-
-    Returns
-    -------
-    downbeats : np.ndarray
-        time points of downbeats in audio file
-    """
-    proc = DBNDownBeatTrackingProcessor(beats_per_bar=[3, 4], fps=100)
-
-    act = RNNDownBeatProcessor()(audio)
-
-    proc_res = proc(act)
-
-    downbeats = proc_res[proc_res[:, 1] == 1, 0]
-    # beat_curve = proc_res[:, 0]
-    return downbeats
-
-
-def fade_track(audio: np.ndarray, fade_duration: int, mode: str = "in") -> np.ndarray:
-    """
-    Fade an audio track in or out with a linear envelope.
-
-    Parameters
-    ----------
-    audio : np.ndarray
-        mono representation of audio file
-    fade_duration : int
-        number of samples over which fade will be applied
-    mode : str
-        If 'in', amplitude of audio file will increase from 0 to 1 over fade duration
-        If 'out' amplitude of audio file decrease from 1 to 0 over fade duration
-    """
-    if mode not in ["in", "out"]:
-        raise ValueError("Mode must be 'in' or 'out'.")
-
-    if mode == "in":
-        start_sample = 0
-        end_sample = fade_duration
-        fade_envelope = np.linspace(0, 1, fade_duration)
-    else:
-        start_sample = len(audio) - fade_duration
-        end_sample = len(audio)
-        fade_envelope = np.linspace(1, 0, fade_duration)
-
-    audio[start_sample:end_sample] *= fade_envelope
-
-    return audio
 
 
 if __name__ == "__main__":
---
+++ mixer/track.py
@@ -0,0 +1,166 @@
+import pathlib
+from typing import Optional
+
+import essentia.standard as es
+import numpy as np
+import pyrubberband as pyrb
+from madmom.features.downbeats import DBNDownBeatTrackingProcessor, RNNDownBeatProcessor
+
+SAMPLE_RATE = 44100  # Sample rate fixed for essentia
+
+
+class Track:
+    SAMPLE_RATE = SAMPLE_RATE
+
+    def __init__(self, file_path: str) -> None:
+        """
+        Parameters
+        ----------
+        file_path : str
+            absolute or relative location of track audio file
+        """
+        self._file_path = pathlib.Path(file_path)
+        self._name = self._file_path.stem
+
+        self._audio = np.array([])
+        self._bpm = None
+        self._downbeats = np.array([])
+
+    @property
+    def audio(self) -> np.ndarray:
+        return self._audio
+
+    @property
+    def downbeats(self) -> np.ndarray:
+        return self._downbeats
+
+    @property
+    def bpm(self) -> Optional[float]:
+        return self._bpm
+
+    @bpm.setter
+    def bpm(self, bpm: float) -> np.ndarray:
+        """
+        Time stretch audio file to increase BPM to target
+
+        Parameters
+        ----------
+        bpm : float
+            intended BPM of audio
+
+        Returns
+        -------
+        np.ndarray
+            time-stretched audio
+        """
+        if self._bpm is None:
+            self.calculate_bpm()
+
+        assert self._bpm is not None
+
+        stretch_factor = bpm / self._bpm
+        self._audio = pyrb.time_stretch(self._audio, SAMPLE_RATE, stretch_factor)
+        self.calculate_bpm()
+
+        return self._audio
+
+    def load(self, path: Optional[str] = None) -> np.ndarray:
+        """
+        Load an audio file from a given path.
+
+        Parameters
+        ----------
+        path : Optional[str]
+            local path to audio file
+            if None, file_path attribute value used
+
+        Returns
+        -------
+        np.ndarray
+            mono representation of audio file
+        """
+        if path is None:
+            path = str(self._file_path.resolve())
+
+        loader = es.MonoLoader(filename=path, sampleRate=SAMPLE_RATE)
+        self._audio = loader()
+
+        return self._audio
+
+    def crop(self, offset: int, length: int) -> None:
+        """
+        Crop track using number of downbeats.
+
+        Parameters
+        ----------
+        offset : int
+            number of downbeats into original audio to crop from
+        length : int
+            number of downbeats that new audio will contain
+        """
+        if self.downbeats.size == 0:
+            self.calculate_downbeats()
+
+        start_sample = int(self._downbeats[offset] * SAMPLE_RATE)
+        end_sample = int(self._downbeats[offset + length] * SAMPLE_RATE)
+
+        self._audio = self._audio[start_sample : end_sample + 1]
+
+    def calculate_bpm(self) -> float:
+        """
+        Determine BPM for audio using essentia
+
+        Returns
+        -------
+        bpm : float
+            tempo of audio file
+        """
+        rhythm_extractor = es.RhythmExtractor2013(method="degara")
+        self._bpm, _, _, _, _ = rhythm_extractor(self._audio)
+
+        assert self._bpm is not None
+
+        return self._bpm
+
+    def calculate_downbeats(self) -> None:
+        """
+        Use madmom downbeat tracking to estimate downbeat time points for audio file.
+        """
+        proc = DBNDownBeatTrackingProcessor(beats_per_bar=[3, 4], fps=100)
+        act = RNNDownBeatProcessor()(self._audio)
+        proc_res = proc(act)
+
+        self._downbeats = proc_res[proc_res[:, 1] == 1, 0]
+
+
+class TrackGroup:
+    def __init__(self) -> None:
+        self._tracks: list[Track] = []
+        self._bpm: Optional[float] = None
+
+    @property
+    def bpm(self) -> Optional[float]:
+        return self._bpm
+
+    @property
+    def tracks(self) -> list[Track]:
+        return self._tracks
+
+    def add_track(self, track: Track) -> None:
+        """
+        Add a track to the track group.
+
+        Parameters
+        ----------
+        track : Track
+            track to be added
+        """
+        self._tracks.append(track)
+        self.calculate_bpm()
+
+    def calculate_bpm(self):
+        """
+        Calculate average bpm of current tracks in group.
+        """
+        track_bpms = [track.bpm for track in self._tracks]
+        self._bpm = sum(track_bpms) / len(track_bpms)
